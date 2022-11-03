8c3170dce6e1183d9eae6689c6791a32bf0c9595
Geova Jonnatha Correia da Silva <geova_jonnatha@hotmail.com>
2022-11-03 20:45:05 -0300

    update

---
+++ front/vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // aqui eu configurei as informações do manifest
+    VitePWA({
+      // registerType: "autoUpdate",
+      // injectRegister: "auto",
+
+      // se eu quiser apontar para onde ficaria meu service work
+      // eu desabilito as 3 linhas abaixo
+      // strategies: "injectManifest",
+      // srcDir: "src",
+      // filename: "my_sw.js",
+      manifest: {
+        lang: "ptBR",
+        name: "webcode",
+        short_name: "webcode",
+        theme_color: "#5ac8fb",
+        background_color: "#5AC8FB",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
